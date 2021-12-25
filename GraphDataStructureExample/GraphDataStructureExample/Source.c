3fd570cb6596850c9e3221fd3ecbbcf6f1487ffe
guiquadros <guiquadros35@gmail.com>
2021-12-25 19:29:42 -0300

    DFS start

--- GraphDataStructureExample/GraphDataStructureExample/Source.c
+++ GraphDataStructureExample/GraphDataStructureExample/Source.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// total number of vertexes of the testing graph
+#define NUMBER_OF_VERTEXES 6 
+
 struct neighbor_node
 {
 	int vertex;
@@ -10,16 +13,20 @@ struct neighbor_node
 struct graph
 {
 	int total_vertexes;
-	struct neighbor_node **adjacency_lists; // list (with the size of total_vertexes) of Adjacency List (each Adjacency List represents the neighbors of each vertex)
+
+	// list (with the size of total_vertexes) of Adjacency List (each Adjacency List represents the neighbors of each vertex)
+	struct neighbor_node **adjacency_lists;
 };
 
 struct graph* create_graph(int total_vertexes);
 void add_edge(struct graph* graph, int origin, int destiny);
 void show_graph_adjacency_lists(struct graph* graph);
+void search_and_display_vertexes_with_dfs(struct graph* graph, int v, int visited_vertexes[]);
 
 int main()
 {
-	struct graph* test_graph = create_graph(6);
+	// creates a graph with total_vertexes = NUMBER_OF_VERTEXES
+	struct graph* test_graph = create_graph(NUMBER_OF_VERTEXES);
 
 	// vertex 0
 	add_edge(test_graph, 0, 1);
@@ -45,7 +52,20 @@ int main()
 	// vertex 5
 	//add_edge(test_graph, 5, 3); // --> no need since we always create the link on both sides, already added with: add_edge(test_graph, 3, 5);
 
+	// shows the whole graph with its adjacency lists
 	show_graph_adjacency_lists(test_graph);
+
+	// reads the target_vertex to start the DFS
+	printf("\nInform a vertex to start the DFS (depth-first search) to display the whole graph visiting only one vertex at a time: ");
+	int starting_vertex;
+	scanf_s("%d", &starting_vertex);
+
+	// stores the visited vertex during the search, 0 = not visited, 1 = visited. Default value = 0 for all positions.
+	int visited_vertexes[NUMBER_OF_VERTEXES] = { 0 }; 
+
+	// searches starting with the target_vertex in the graph using a DFS (depth-first search) algorithm ("busca em profundidade" in pt-br)
+	//and display each vertex visited until it finishes the output of the whole graph
+	search_and_display_vertexes_with_dfs(test_graph, starting_vertex, visited_vertexes);
 
 	system("pause");
 	return 0;
@@ -95,5 +115,17 @@ void show_graph_adjacency_lists(struct graph* graph)
 		}
 
 		printf("\n\n");
+	}
+}
+
+void search_and_display_vertexes_with_dfs(struct graph* graph, int v, int visited_vertexes[])
+{
+	struct neighbor_node *neighbor = graph->adjacency_lists[v];
+
+	while (neighbor != NULL)
+	{
+		// TODO
+
+		neighbor = neighbor->next;
 	}
 }
